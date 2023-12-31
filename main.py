3e9e6dbe1d0b695818eee9ee4882a614665a20f8
undefined06855 <currentcashew57@gmail.com>
2023-12-31 12:38:41 +0000

    dpad

--- main.py
+++ main.py
@@ -5,6 +5,22 @@ def main():
     while 1:
         events = get_gamepad()
         for event in events:
+            if event.ev_type == "Absolute":
+                # dpad movement
+                if event.code == "ABS_X":
+                    if event.state == 0:
+                        print("d-left")
+                    elif event.state == 127:
+                        print("d-center")
+                    elif event.state == 256:
+                        print("d-right")
+                if event.code == "ABS_Y":
+                    if event.state == 0:
+                        print("d-up")
+                    elif event.state == 127:
+                        print("d-center")
+                    elif event.state == 256:
+                        print("d-down")
             print(event.ev_type, event.code, event.state)
 
 
