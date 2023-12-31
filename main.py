f658bafd5db4c6bbfb70c00f994accd9cc8d562e
undefined06855 <currentcashew57@gmail.com>
2023-12-31 23:00:48 +0000

    change stuff and stuff

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ subprocess.run(["sudo", "hciconfig", "hci0", "piscan"])
 
 # Set up Bluetooth device information
 device_name = "SNESController"
-service_uuid = "94f39d29-7d6d-437d-973b-fba39e49d4ee"
+service_uuid = "77a1fabd-be72-41c5-888d-fa1dd471c184"
 
 # Create Bluetooth socket
 server_socket = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
@@ -15,7 +15,7 @@ server_socket.bind(("", bluetooth.PORT_ANY))
 server_socket.listen(1)
 
 # Advertise the Bluetooth service
-bluetooth.advertise_service(server_socket, "SNESController Service",
+bluetooth.advertise_service(server_socket, device_name,
                             service_id=service_uuid,
                             service_classes=[service_uuid, bluetooth.SERIAL_PORT_CLASS],
                             profiles=[bluetooth.SERIAL_PORT_PROFILE])
