2a5c9e3885ab4738fa370fc6b6cd955e9e6b351e
undefined06855 <currentcashew57@gmail.com>
2023-12-31 19:38:42 +0000

    stuff

--- main.py
+++ main.py
@@ -1,50 +1,92 @@
-from inputs import get_gamepad
+"""
+Example for a BLE 4.0 Server
+"""
 
-def main():
-    """Just print out some event infomation when the gamepad is used."""
-    while 1:
-        for event in get_gamepad():
-            if event.ev_type == "Absolute":
-                # dpad movement
-                if event.code == "ABS_X":
-                    if event.state == 0:
-                        print("d-left")
-                    elif event.state == 127:
-                        print("d-center")
-                    elif event.state == 255:
-                        print("d-right")
-                if event.code == "ABS_Y":
-                    if event.state == 0:
-                        print("d-up")
-                    elif event.state == 127:
-                        print("d-center")
-                    elif event.state == 255:
-                        print("d-down")
-                
-            if event.ev_type == "Key":
-                # face buttons (can also test using Misc, but is a bit misleading)
-                if event.code == "BTN_THUMB":
-                    print("A")
-                elif event.code == "BTN_THUMB2":
-                    print("B")
-                elif event.code == "BTN_TRIGGER":
-                    print("X")
-                elif event.code == "BTN_TOP":
-                    print("Y")
-                
-                # bumpers
-                elif event.code == "BTN_TOP2":
-                    print("L")
-                elif event.code == "BTN_PINKIE":
-                    print("R")
+import logging
+import asyncio
+import threading
 
-                # start / select
-                elif event.code == "BTN_BASE4":
-                    print("Start")
-                elif event.code == "BTN_BASE3":
-                    print("Select")
-            print("                                 ", event.ev_type, event.code, event.state)
+from typing import Any
+
+from bless import (  # type: ignore
+        BlessServer,
+        BlessGATTCharacteristic,
+        GATTCharacteristicProperties,
+        GATTAttributePermissions
+        )
+
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(name=__name__)
+trigger: threading.Event = threading.Event()
 
 
-if __name__ == "__main__":
-    main()
+def read_request(
+        characteristic: BlessGATTCharacteristic,
+        **kwargs
+        ) -> bytearray:
+    logger.debug(f"Reading {characteristic.value}")
+    return characteristic.value
+
+
+def write_request(
+        characteristic: BlessGATTCharacteristic,
+        value: Any,
+        **kwargs
+        ):
+    characteristic.value = value
+    logger.debug(f"Char value set to {characteristic.value}")
+    if characteristic.value == b'\x0f':
+        logger.debug("NICE")
+        trigger.set()
+
+
+async def run(loop):
+    trigger.clear()
+    # Instantiate the server
+    my_service_name = "Test Service"
+    server = BlessServer(name=my_service_name, loop=loop)
+    server.read_request_func = read_request
+    server.write_request_func = write_request
+
+    # Add Service
+    my_service_uuid = "A07498CA-AD5B-474E-940D-16F1FBE7E8CD"
+    await server.add_new_service(my_service_uuid)
+
+    # Add a Characteristic to the service
+    my_char_uuid = "51FF12BB-3ED8-46E5-B4F9-D64E2FEC021B"
+    char_flags = (
+            GATTCharacteristicProperties.read |
+            GATTCharacteristicProperties.write |
+            GATTCharacteristicProperties.indicate
+            )
+    permissions = (
+            GATTAttributePermissions.readable |
+            GATTAttributePermissions.writeable
+            )
+    await server.add_new_characteristic(
+            my_service_uuid,
+            my_char_uuid,
+            char_flags,
+            None,
+            permissions)
+
+    logger.debug(
+            server.get_characteristic(
+                my_char_uuid
+                )
+            )
+    await server.start()
+    logger.debug("Advertising")
+    logger.info(f"Write '0xF' to the advertised characteristic: {my_char_uuid}")
+    trigger.wait()
+    await asyncio.sleep(2)
+    logger.debug("Updating")
+    server.get_characteristic(my_char_uuid)
+    server.update_value(
+            my_service_uuid, "51FF12BB-3ED8-46E5-B4F9-D64E2FEC021B"
+            )
+    await asyncio.sleep(5)
+    await server.stop()
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(run(loop))
---
+++ test.py
@@ -0,0 +1,50 @@
+from inputs import get_gamepad
+
+def main():
+    """Just print out some event infomation when the gamepad is used."""
+    while True:
+        for event in get_gamepad():
+            if event.ev_type == "Absolute":
+                # dpad movement
+                if event.code == "ABS_X":
+                    if event.state == 0:
+                        print("d-left")
+                    elif event.state == 127:
+                        print("d-center")
+                    elif event.state == 255:
+                        print("d-right")
+                if event.code == "ABS_Y":
+                    if event.state == 0:
+                        print("d-up")
+                    elif event.state == 127:
+                        print("d-center")
+                    elif event.state == 255:
+                        print("d-down")
+                
+            if event.ev_type == "Key":
+                # face buttons (can also test using Misc, but is a bit misleading)
+                if event.code == "BTN_THUMB":
+                    print("A")
+                elif event.code == "BTN_THUMB2":
+                    print("B")
+                elif event.code == "BTN_TRIGGER":
+                    print("X")
+                elif event.code == "BTN_TOP":
+                    print("Y")
+                
+                # bumpers
+                elif event.code == "BTN_TOP2":
+                    print("L")
+                elif event.code == "BTN_PINKIE":
+                    print("R")
+
+                # start / select
+                elif event.code == "BTN_BASE4":
+                    print("Start")
+                elif event.code == "BTN_BASE3":
+                    print("Select")
+            print("                                 ", event.ev_type, event.code, event.state)
+
+
+if __name__ == "__main__":
+    main()
