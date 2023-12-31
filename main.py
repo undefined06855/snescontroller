69438bab2b2408a82f0b0f95e420bc4efca2719e
undefined06855 <currentcashew57@gmail.com>
2023-12-31 12:30:28 +0000

    first commit

---
+++ main.py
@@ -0,0 +1,12 @@
+from inputs import get_gamepad
+
+def main():
+    """Just print out some event infomation when the gamepad is used."""
+    while 1:
+        events = get_gamepad()
+        for event in events:
+            print(event.ev_type, event.code, event.state)
+
+
+if __name__ == "__main__":
+    main()
