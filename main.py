4427fa16fc6712c9c02181f462ef4eb0f80fc25a
undefined06855 <currentcashew57@gmail.com>
2023-12-31 21:58:21 +0000

    advertise device

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from bluetooth import BluetoothSocket, RFCOMM
+
+from bluetooth import BluetoothSocket, RFCOMM, advertise_service
 import time
 
 def start_emulator():
@@ -6,13 +7,16 @@ def start_emulator():
     server_socket.bind(("", 1))
     server_socket.listen(1)
 
+    # Set the device name and advertise the service
+    advertise_service(server_socket, "RaspberryPiEmulator", service_id="12345678-1234-5678-1234-56789abcdef1")
+
     print("Emulator waiting for connection...")
 
     client_socket, address = server_socket.accept()
     print(f"Accepted connection from {address}")
 
     data = "Hello, Web Bluetooth!"
-    
+
     while True:
         client_socket.send(data)
         time.sleep(1)
