492e4d69498516fc8acf9bf9bcdfb885e8c8bedd
undefined06855 <currentcashew57@gmail.com>
2023-12-31 22:18:40 +0000

    redo everythin

--- main.py
+++ main.py
@@ -1,27 +1,44 @@
-import subprocess
-from bluetooth import BluetoothSocket, RFCOMM
+import bluetooth
 import time
 
-def set_device_name(new_name):
-    subprocess.run(["sudo", "hciconfig", "hci0", "name", new_name])
+def emulate_bluetooth_device():
+    # Set the Bluetooth device name
+    device_name = "SNESController"
 
-def start_emulator():
-    set_device_name("RaspberryPiEmulator")
-
-    server_socket = BluetoothSocket(RFCOMM)
-    server_socket.bind(("", 1))
+    # Create a Bluetooth socket
+    server_socket = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+    server_socket.bind(("", bluetooth.PORT_ANY))
     server_socket.listen(1)
 
-    print("Emulator waiting for connection...")
+    # Advertise the service
+    bluetooth.advertise_service(server_socket, "SNESControllerService", service_id=bluetooth.RFCOMM)
 
-    client_socket, address = server_socket.accept()
-    print(f"Accepted connection from {address}")
+    print(f"Emulating Bluetooth device: {device_name}")
 
-    data = "Hello, Web Bluetooth!"
+    try:
+        while True:
+            print("Waiting for connection...")
+            client_socket, address = server_socket.accept()
+            print(f"Accepted connection from {address}")
 
-    while True:
-        client_socket.send(data)
-        time.sleep(1)
+            try:
+                while True:
+                    # Send data to the connected device
+                    data = "Hello, SNES!"
+                    client_socket.send(data)
+
+                    time.sleep(1)  # Adjust as needed
+
+            except KeyboardInterrupt:
+                pass
+            finally:
+                client_socket.close()
+
+    except KeyboardInterrupt:
+        pass
+    finally:
+        server_socket.close()
+        print("Bluetooth emulation stopped.")
 
 if __name__ == "__main__":
-    start_emulator()
+    emulate_bluetooth_device()
