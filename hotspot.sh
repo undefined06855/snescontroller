4f582c36aa1bb4a3ecb013ada5b9be1839ec8e10
undefined06855 <currentcashew57@gmail.com>
2024-01-01 17:07:41 +0000

    more fixes, no pass

--- hotspot.sh
+++ hotspot.sh
@@ -1,10 +1,11 @@
+#!/bin/bash
+
 # Set hotspot name and password
 hotspot_name="SNESController"
 hotspot_password="snescontroller"
 
-
 # Log file path
-log_file="./log2.txt"  # Replace with the actual path
+log_file="./log3.txt"  # Replace with the actual path
 
 # Redirect all output to the log file
 exec &> "$log_file"
@@ -27,7 +28,7 @@ sudo iwconfig wlan0 mode ad-hoc
 
 # Set the IBSS parameters
 sudo iwconfig wlan0 essid "$hotspot_name" mode ad-hoc
-sudo iwconfig wlan0 key s:"$hotspot_password"
+sudo iwconfig wlan0 key off  # Disable encryption for ad-hoc
 sudo iwconfig wlan0 channel 6
 
 # Bring wlan0 back up
