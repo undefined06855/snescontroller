aeda327e859214bf36204f293e30bd1c31a606f5
undefined06855 <currentcashew57@gmail.com>
2024-01-01 16:39:35 +0000

    stufffffffffffffff

--- main.py
+++ main.py
@@ -1,19 +1,25 @@
 import os
+import time
 
 # Set hotspot name and password
 hotspot_name = "SNESController"
 hotspot_password = "snescontroller"
 
-# Command to create hotspot
+# Commands to create hotspot
 commands = [
     "sudo systemctl stop dhcpcd",
+    "sudo systemctl stop wpa_supplicant",
     "sudo ip link set wlan0 down",
+    f"sudo iw dev wlan0 set type ibss",
     f"sudo iw dev wlan0 ibss join {hotspot_name} {hotspot_password} freq 2437",
     "sudo ip link set wlan0 up",
-    "sudo systemctl start dhcpcd"
+    "sudo systemctl start dhcpcd",
+    "sudo systemctl start wpa_supplicant"
 ]
 
-# Execute the command
+# Execute the commands
 for command in commands:
     print("Executing: " + command)
     os.system(command)
+    # Add a small delay between commands
+    time.sleep(1)
