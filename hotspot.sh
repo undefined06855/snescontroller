d0fc8b1b7c8887d29c2d147a0be5974cfec2a932
undefined06855 <currentcashew57@gmail.com>
2024-01-01 16:48:47 +0000

    yeh

--- hotspot.sh
+++ hotspot.sh
@@ -1,12 +1,31 @@
 #!/bin/bash
-# doesnt work!
-echo THIS DOESN'T WORK!!
 
+# Set hotspot name and password
+hotspot_name="SNESController"
+hotspot_password="snescontroller"
+
+# Stop services
 sudo systemctl stop dhcpcd
 sudo systemctl stop wpa_supplicant
+
+# Bring down wlan0
 sudo ip link set wlan0 down
-sudo iw dev wlan0 set type ibss
-sudo iw dev wlan0 ibss join SNESController snescontroller freq 2437
+
+# Unload the wireless driver
+sudo rmmod brcmfmac
+sudo rmmod brcmutil
+
+# Load the wireless driver with the correct mode
+sudo modprobe brcmfmac
+sudo modprobe brcmutil
+sudo iwconfig wlan0 mode ad-hoc
+
+# Set the IBSS parameters
+sudo iwconfig wlan0 essid "$hotspot_name" mode ad-hoc key "$hotspot_password" channel 6
+
+# Bring wlan0 back up
 sudo ip link set wlan0 up
+
+# Start services
 sudo systemctl start dhcpcd
 sudo systemctl start wpa_supplicant
