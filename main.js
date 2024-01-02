f1c6c6eefc723f840eaa47d2c340ae64fd81ab57
undefined06855 <98057141+undefined06855@users.noreply.github.com>
2024-01-02 22:13:10 +0000

    joypad.js

--- main.js
+++ main.js
@@ -1,24 +1,15 @@
 const WebSocketServer = require("ws").WebSocketServer
-const gamepad = require("gamepad")
+const joypad = require("joypad.js")
 
-gamepad.init()
+joypad.on("button_press", (e) => {
+    const { buttonName } = e.detail
+  
+    console.log(`${buttonName} was pressed!`)
+})
 
-// poll events at a very high rate
-setInterval(gamepad.processEvents, 10)
-   
-// Listen for button up events on all gamepads
-gamepad.on("up", function (id, num) {
-    console.log("up", {
-      id: id,
-      num: num,
-    });
-  });
-   
-  // Listen for button down events on all gamepads
-  gamepad.on("down", function (id, num) {
-    console.log("down", {
-      id: id,
-      num: num,
-    });
-  });
 
+joypad.on("button_release", (e) => {
+    const { buttonName } = e.detail
+  
+    console.log(`${buttonName} was released!`)
+})
