6c2cd861108aaf6adabd4b692873422c9e1de28a
undefined06855 <98057141+undefined06855@users.noreply.github.com>
2024-01-02 16:39:39 +0000

    maybe changing the order will help?

--- web/startup.sh
+++ web/startup.sh
@@ -1,6 +1,7 @@
 sudo rm *.log
 git pull origin main            &> gitpull.log
 # comment out the next few lines to enable ssh and disable the controller hotspot
-sudo bash ./create_hotspot.sh   &> hotspotcreate.log
-echo true                       &> conformation.log
+echo running python...
 sudo python -u ./web.py         &> pythonwebstuff.log
+sudo bash ./create_hotspot.sh   &> hotspotcreate.log
+
