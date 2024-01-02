917bd67cd8512813c447aa19970f31c0cf92b78e
undefined06855 <98057141+undefined06855@users.noreply.github.com>
2024-01-02 22:03:05 +0000

    mmm yeuh

--- main.js
+++ main.js
@@ -1,4 +1,24 @@
 const WebSocketServer = require("ws").WebSocketServer
 const gamepad = require("gamepad")
 
+gamepad.init()
+
+// poll events at a very high rate
+setInterval(gamepad.processEvents, 10)
+   
+// Listen for button up events on all gamepads
+gamepad.on("up", function (id, num) {
+    console.log("up", {
+      id: id,
+      num: num,
+    });
+  });
+   
+  // Listen for button down events on all gamepads
+  gamepad.on("down", function (id, num) {
+    console.log("down", {
+      id: id,
+      num: num,
+    });
+  });
 
