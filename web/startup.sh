396ee4399dc593961b91b3c05b2c6288013599ce
undefined06855 <98057141+undefined06855@users.noreply.github.com>
2024-01-02 15:53:58 +0000

    add logging

--- web/startup.sh
+++ web/startup.sh
@@ -1,4 +1,4 @@
-git pull origin main
+git pull origin main &> gitpull.log
 # comment out the next line to enable ssh and disable the controller hotspot
-sudo bash ./create_hotspot.sh
-sudo python ./web.py
+sudo bash ./create_hotspot.sh &> hotspotcreate.log
+sudo python ./web.py &> python.log
