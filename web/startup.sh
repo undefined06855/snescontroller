d8687addc0de61eba883d80c363a4f6f9ca08fc3
undefined06855 <98057141+undefined06855@users.noreply.github.com>
2024-01-02 16:29:46 +0000

    unbuffer and redo stuff

--- web/startup.sh
+++ web/startup.sh
@@ -2,4 +2,4 @@ sudo rm *.log
 git pull origin main &> gitpull.log
 # comment out the next line to enable ssh and disable the controller hotspot
 sudo bash ./create_hotspot.sh &> hotspotcreate.log
-sudo python ./web.py &> python.log
+sudo python -u ./web.py &> pythonwebstuff.log
