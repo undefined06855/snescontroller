3b24a34c53d9a104b9b958a7bbadc640a64ec668
undefined06855 <98057141+undefined06855@users.noreply.github.com>
2024-01-02 16:38:45 +0000

    why are LOGS NOT WORKINGGGGGGGGGG

--- web/startup.sh
+++ web/startup.sh
@@ -1,5 +1,6 @@
 sudo rm *.log
-git pull origin main &> gitpull.log
-# comment out the next line to enable ssh and disable the controller hotspot
-sudo bash ./create_hotspot.sh &> hotspotcreate.log
-sudo python -u ./web.py &> pythonwebstuff.log
+git pull origin main            &> gitpull.log
+# comment out the next few lines to enable ssh and disable the controller hotspot
+sudo bash ./create_hotspot.sh   &> hotspotcreate.log
+echo true                       &> conformation.log
+sudo python -u ./web.py         &> pythonwebstuff.log
