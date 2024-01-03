d3360dc71ed38edd21707ffb795c5a6667a9c4d7
undefined06855 <98057141+undefined06855@users.noreply.github.com>
2024-01-03 20:16:04 +0000

    NOOO I BROKE IT :((((

--- new/startup.sh
+++ new/startup.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 echo Running...
 
-exit 0
-
 # make sure to do this before running the rest, else you'd need to actually take
 # out the sd card to change anything! I hope this works...
+# edit: IT FUCKING DIDNT I THINK I BROKE IT AAAAAAAAAAAAAAAAAARGH
 git pull origin main
+
 
 sudo systemctl stop dhcpcd
 sudo systemctl stop wpa_supplicant
@@ -41,4 +41,4 @@ sudo dnsmasq -C /etc/dnsmasq.conf -d &
 
 sleep 2
 
-node main.js
+node main.js &
